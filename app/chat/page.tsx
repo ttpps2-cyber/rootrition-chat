5903a0875b34a945c39e526a6ccaec293cd6fdf5
법재 <ttpps2@gmail.com>
2026-05-09 09:12:10 +0900

    feat: add chat page and root layout with disclaimer footer

---
+++ app/chat/page.tsx
@@ -0,0 +1,9 @@
+import ChatWindow from '@/components/ChatWindow'
+
+export default function ChatPage() {
+  return (
+    <div className="flex-1 flex flex-col overflow-hidden">
+      <ChatWindow />
+    </div>
+  )
+}
